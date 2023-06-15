aa9715680dbf7c6506bf30eb27d41219028e5d2e
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-06-14 19:20:33 -0500

    intro to node

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,1 +1,10 @@
 //Make an api request using async await
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(res)
+    console.log(data)
+    document.querySelector("img").src = data.message
+}
+
+document.querySelector("button").addEventListener("click", getACuteDogPhoto)
--- async-practice/js/main.js
+++ async-practice/js/main.js
@@ -111,44 +111,47 @@
 //     .catch(err => console.log(err))
 
 //Code 07
-function houseOne(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 1')
-        }, 1000)
-    })
-}
-function houseTwo(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 2')
-        }, 5000)
-    })
-}
-function houseThree(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 3')
-        }, 2000)
-    })
-}
+// function houseOne(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 1')
+//         }, 1000)
+//     })
+// }
+// function houseTwo(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 2')
+//         }, 5000)
+//     })
+// }
+// function houseThree(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 3')
+//         }, 2000)
+//     })
+// }
 
-async function getPaid(){
-    const houseOneWait = await houseOne()
-    const houseTwoWait = await houseTwo()
-    const houseThreeWait = await houseThree()
-    console.log(houseOneWait)
-    console.log(houseTwoWait)
-    console.log(houseThreeWait)
-}
+// async function getPaid(){
+//     const houseOneWait = await houseOne()
+//     const houseTwoWait = await houseTwo()
+//     const houseThreeWait = await houseThree()
+//     console.log(houseOneWait)
+//     console.log(houseTwoWait)
+//     console.log(houseThreeWait)
+// }
 
-getPaid()
+// getPaid()
 
 //Code 08
 async function getACuteDogPhoto(){
     const res = await fetch('https://dog.ceo/api/breeds/image/random')
     const data = await res.json()
     console.log(data)
+    document.querySelector("img").src = data.message
 }
 getACuteDogPhoto()
+
+
 
